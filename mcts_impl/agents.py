decd2a027d33e76233841a2f7aab1f017ab17ab6
Paweł Szmergała <pawelszme@gmail.com>
2022-02-03 23:51:31 +0100

    Simplify imports and rename `mcts` package

--- mcts_impl/agents.py
+++ mcts_impl/agents.py
@@ -1,6 +1,5 @@
-import __init__
-from mcts import MCTS, AbstractEvaluator
-from game import State
+from .mcts import MCTS, AbstractEvaluator
+from .game import State
 from models.logreg import LogisticRegression
 from utilities import *
 from numpy import array
